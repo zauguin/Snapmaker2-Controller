3a5fc334453f8e9b32ab976c44eaf113ac4479c3
Marcel Fabian Krüger <snapmaker@2krueger.de>
2020-12-26 10:06:39 +0100

    Make some deaults in FreeRTOSConfig.h explicit

--- snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
+++ snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
@@ -48,25 +48,42 @@
 #endif
 
 #define configUSE_PREEMPTION			1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
+#define configUSE_TICKLESS_IDLE			0
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( F_CPU )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 4 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 120 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 36 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			0
+#define configUSE_TASK_NOTIFICATIONS	1
 #define configUSE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	2
 #define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	1
-#define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	1
+#define configQUEUE_REGISTRY_SIZE		8
+#define configUSE_QUEUE_SETS			0
+#define configUSE_TIME_SLICING			1
+#define configUSE_NEWLIB_REENTRANT		0
+#define configENABLE_BACKWARD_COMPATIBILITY	1
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS	0
+#define configSTACK_DEPTH_TYPE			uint16_t
+#define configMESSAGE_BUFFER_LENGTH_TYPE	size_t
+#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configUSE_MALLOC_FAILED_HOOK	1
+#define configUSE_DAEMON_TASK_STARTUP_HOOK	0
+#define configUSE_APPLICATION_TASK_TAG	0
 #define configGENERATE_RUN_TIME_STATS	0
+#define configUSE_STATS_FORMATTING_FUNCTIONS	0
+
+/* Memory allocation related definitions. */
+#define configSUPPORT_STATIC_ALLOCATION		0
+#define configSUPPORT_DYNAMIC_ALLOCATION	1
+#define configTOTAL_HEAP_SIZE				( ( size_t ) ( 36 * 1024 ) )
+#define configAPPLICATION_ALLOCATED_HEAP	0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
